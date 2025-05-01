39eece57a208234780e7fa1364f9fcbbb4794a21
Gabriel Moura <gabrielpratamg.gm@gmail.com>
2025-05-01 17:49:10 -0300

    First Commit - Fokus

---
+++ app/script-crud.js
@@ -0,0 +1,146 @@
+const btnAddTask = document.querySelector(".app__button--add-task");
+const formAddTask = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+const ulTasks = document.querySelector(".app__section-task-list");
+const btnCancel = document.querySelector(".app__form-footer__button--cancel");
+const taskDescriptionParagraph = document.querySelector(
+  ".app__section-active-task-description"
+);
+
+const btnRemoveCompleteTasks = document.querySelector(
+  "#btn-remover-concluidas"
+);
+const btnRemoveAllTasks = document.querySelector("#btn-remover-todas");
+
+let toDoList = JSON.parse(localStorage.getItem("tasks")) || [];
+let selectedTask = null;
+let liSelectdTask = null;
+
+function clearForm() {
+  textarea.value = "";
+  formAddTask.classList.add("hidden");
+}
+
+btnCancel.addEventListener("click", clearForm);
+
+function uptadeTask() {
+  localStorage.setItem("tasks", JSON.stringify(toDoList));
+}
+
+function createTaskElement(task) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svg = document.createElement("svg");
+  svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
+  `;
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent = task.description;
+  paragraph.classList.add("app__section-task-list-item-description");
+
+  const button = document.createElement("button");
+  button.classList.add("app_button-edit");
+
+  button.onclick = () => {
+    const newDescription = prompt("Qual é o novo nome da tarefa?");
+    if (!newDescription) {
+      alert("É necessário inserir um novo nome para a nova tarefa.");
+      return;
+    } else {
+      paragraph.textContent = newDescription;
+      task.description = newDescription;
+      uptadeTask();
+    }
+  };
+
+  const buttonImage = document.createElement("img");
+  buttonImage.setAttribute("src", "/assets/edit.png");
+  button.append(buttonImage);
+
+  li.append(svg);
+  li.append(paragraph);
+  li.append(button);
+
+  if (task.complete) {
+    li.classList.add("app__section-task-list-item-complete");
+    button.setAttribute("disabled", "disabled");
+  } else {
+    li.onclick = () => {
+      document
+        .querySelectorAll(".app__section-task-list-item-active")
+        .forEach((elemento) => {
+          elemento.classList.remove("app__section-task-list-item-active");
+        });
+      if (selectedTask == task) {
+        taskDescriptionParagraph.textContent = "";
+        selectedTask = null;
+        liSelectdTask = null;
+        return;
+      }
+      selectedTask = task;
+      liSelectdTask = li;
+      taskDescriptionParagraph.textContent = task.description;
+
+      li.classList.add("app__section-task-list-item-active");
+    };
+  }
+
+  return li;
+}
+
+btnAddTask.addEventListener("click", () => {
+  formAddTask.classList.toggle("hidden");
+});
+
+formAddTask.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const task = {
+    description: textarea.value,
+  };
+  toDoList.push(task);
+  const taskElement = createTaskElement(task);
+  ulTasks.append(taskElement);
+  uptadeTask();
+  textarea.value = "";
+  formAddTask.classList.add("hidden");
+});
+
+toDoList.forEach((task) => {
+  const taskElement = createTaskElement(task);
+  ulTasks.append(taskElement);
+});
+
+document.addEventListener("FocusFinished", () => {
+  if (selectedTask && liSelectdTask) {
+    liSelectdTask.classList.remove("app__section-task-list-item-active");
+    liSelectdTask.classList.add("app__section-task-list-item-complete");
+    liSelectdTask.querySelector("button").setAttribute("disabled", "disabled");
+    selectedTask.complete = true;
+    uptadeTask();
+  }
+});
+
+const removeTasks = (onlyComplete) => {
+  const selector = onlyComplete
+    ? ".app__section-task-list-item-complete"
+    : ".app__section-task-list-item";
+  document.querySelectorAll(selector).forEach((elemento) => {
+    elemento.remove();
+  });
+  toDoList = onlyComplete ? toDoList.filter((task) => !task.complete) : [];
+  uptadeTask();
+};
+
+btnRemoveCompleteTasks.onclick = () => {
+  removeTasks(true);
+};
+btnRemoveAllTasks.onclick = () => {
+  removeTasks(false);
+};
---
+++ app/script.js
@@ -0,0 +1,126 @@
+const html = document.querySelector("html");
+const focusBt = document.querySelector(".app__card-button--foco");
+const shortBt = document.querySelector(".app__card-button--curto");
+const longBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const title = document.querySelector(".app__title");
+const buttons = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+const focusMusicInput = document.querySelector("#alternar-musica");
+const startOrPauseBt = document.querySelector("#start-pause span");
+const startOrPauseBtIcone = document.querySelector(
+  ".app__card-primary-butto-icon"
+);
+const screenTime = document.querySelector("#timer");
+
+const music = new Audio("/sons/luna-rise-part-one.mp3");
+const audioPlay = new Audio("/sons/play.wav");
+const audioPause = new Audio("/sons/pause.mp3");
+const audioTimeFineshed = new Audio("./sons/beep.mp3");
+
+let elapsedTimeInSecond = 1500;
+let intervalId = null;
+
+music.loop = true;
+
+focusMusicInput.addEventListener("change", () => {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+});
+
+focusBt.addEventListener("click", () => {
+  elapsedTimeInSecond = 1500;
+  changeContext("foco");
+  focusBt.classList.add("active");
+});
+
+shortBt.addEventListener("click", () => {
+  elapsedTimeInSecond = 300;
+  changeContext("descanso-curto");
+  shortBt.classList.add("active");
+});
+
+longBt.addEventListener("click", () => {
+  elapsedTimeInSecond = 900;
+  changeContext("descanso-longo");
+  longBt.classList.add("active");
+});
+
+function changeContext(contexto) {
+  showTime();
+  buttons.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/assets/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      title.innerHTML = `
+              Otimize sua produtividade,<br>
+                  <strong class="app__title-strong">mergulhe no que importa.</strong>
+              `;
+      break;
+    case "descanso-curto":
+      title.innerHTML = `
+              Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+              `;
+      break;
+    case "descanso-longo":
+      title.innerHTML = `
+              Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+              `;
+    default:
+      break;
+  }
+}
+
+const countdown = () => {
+  if (elapsedTimeInSecond <= 0) {
+    audioTimeFineshed.play();
+    alert("Tempo finalizado!");
+    const activeFocus = html.getAttribute("data-contexto") == "foco";
+    if (activeFocus) {
+      const event = new CustomEvent("FocusFinished");
+      document.dispatchEvent(event);
+    }
+    reset();
+    return;
+  }
+  elapsedTimeInSecond -= 1;
+  showTime();
+};
+
+startPauseBt.addEventListener("click", startOrPause);
+
+function startOrPause() {
+  if (intervalId) {
+    audioPause.play();
+    reset();
+    return;
+  }
+  audioPlay.play();
+  intervalId = setInterval(countdown, 1000);
+  startOrPauseBt.textContent = "Pausar";
+  startOrPauseBtIcone.setAttribute("src", `/assets/pause.png`);
+}
+
+function reset() {
+  clearInterval(intervalId);
+  startOrPauseBt.textContent = "Começar";
+  startOrPauseBtIcone.setAttribute("src", `/assets/play_arrow.png`);
+  intervalId = null;
+}
+
+function showTime() {
+  const time = new Date(elapsedTimeInSecond * 1000);
+  const formattedTime = time.toLocaleTimeString("pt-BR", {
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  screenTime.innerHTML = `${formattedTime}`;
+}
+
+showTime();
